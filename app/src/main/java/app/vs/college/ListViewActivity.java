1136ac62520bf5c2f353c352d50765f55543e412
Yubraj Khatri <yubrajkhatri06@gmail.com>
2025-02-10 19:29:09 +0545

    1st commit

---
+++ app/src/main/java/app/vs/college/ListViewActivity.java
@@ -0,0 +1,23 @@
+package app.vs.college;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ListViewActivity  extends AppCompatActivity {
+    private TextView
+
+    @override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_view);
+    }
+
+    @override
+
+    protected void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+    }
+}
